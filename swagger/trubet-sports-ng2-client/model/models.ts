ff4f5e0441b1a04c853032a28551bab1994b09b1
Hudson Graham <hudson.graham@tattsgroup.com>
2016-12-20 16:51:44 +1000

    Remove unwanted files

--- swagger/trubet-sports-ng2-client/model/models.ts
+++
@@ -1,7 +0,0 @@
-export * from './League';
-export * from './MainEvent';
-export * from './Offer';
-export * from './Sport';
-export * from './SportMeeting';
-export * from './SportsData';
-export * from './SubEvent';
